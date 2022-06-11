c510642571d6b790933b22e5e953ff7ecaf9b7af
Zeeshan Ahmad <zeeshnhmd.1@gmail.com>
2022-06-11 13:25:06 +0530

    async function

--- Async - Callback, Promises, Async Await/async.js
+++ Async - Callback, Promises, Async Await/async.js
@@ -1,7 +1,7 @@
 console.log('first');
 /**
- * * settimeout is a asychronous function
- * * eventlistener are also asychronous functions
+ * * settimeout is a asynchronous function
+ * * eventlistener are also asynchronous functions
  * * for clear understanding visit:- http://latentflip.com/loupe/?code=Y29uc29sZS5sb2coJ3N0YXJ0Jyk7CgpzZXRUaW1lb3V0KCgpPT4gewogICAgY29uc29sZS5sb2coJ0ltIGZvcm0gc2V0VGltZU91dCcpOwp9LCA0MDAwKTsKCmNvbnNvbGUubG9nKCdzZWNvbmQnKTs%3D!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
  */
 setTimeout(() => {
